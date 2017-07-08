e25797fde1476847a37d68a59624be47eda32035
liuderchi <liuderchi@gmail.com>
2017-07-08 18:57:48 +0800

    parse links from imgur api response

--- 1. Redux & plain Javascript/v09-thunk middleware-exercise/actions/index.js
+++ 1. Redux & plain Javascript/v09-thunk middleware-exercise/actions/index.js
@@ -23,7 +23,10 @@ var asyncIncrease = function(dispatch, state){
 
 var getRandomImages = function(dispatch, state){
   var imgurAPI = "https://api.imgur.com/3/gallery/random/random/1";
+  // NOTE other API: "https://api.imgur.com/3/gallery/hot/viral/0"
   $.getJSON(imgurAPI).done(function(data){
     console.log('API data: ', data)
+    var links = data.data.map(function(data) { return data.link})
+    console.log(links);
   })
 }
